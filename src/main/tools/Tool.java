f6f667acdef908f0ae923f3254b481e1fa707f02
Wail <wail120@live.fr>
2023-05-28 23:57:43 +0200

    added Player class

--- src/main/tools/Tool.java
+++ src/main/tools/Tool.java
@@ -1,11 +1,27 @@
 package main.tools;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tool {
     public ToolType type;
     int level;
+    public static List<Tool> tools = new ArrayList<>();
     //constructor
-    public Tool(ToolType type){
+    public Tool(ToolType type, int level){
         this.type = type;
-        this.level = 1;
+        this.level = level;
+        tools.add(this);
+    }
+    public static int getlowestlevel(ToolType type){
+        int lowestlevel = 0;
+        for (Tool tool : tools){
+            if (tool.type == type){
+                if (tool.level < lowestlevel){
+                    lowestlevel = tool.level;
+                }
+            }
+        }
+        return lowestlevel;
     }
 }
